db5e7e2dc3fd0287f4addfa6f3134c69c496ba9b
protee1 <inezaprotogene@gmail.com>
2021-08-26 07:37:30 -0700

    Fix 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
-#include <studio.h>
+#include <stdio.h>
 /**
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
-
-Use the function puts
-You are not allowed to use printf
-Your program should end with the value 0
-*/
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+ * main - Prints string
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
