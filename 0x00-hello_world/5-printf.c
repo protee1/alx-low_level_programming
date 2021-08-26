0386fa1c1d364998e6f2f7de4757ee78d5f96995
protee1 <inezaprotogene@gmail.com>
2021-08-26 07:47:16 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/*
+ * Main - Print String
+ * Description: Prints Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+
+Use the function printf
+You are not allowed to use the function puts
+Your program should return 0
+Your program should compile without warning when using the -Wall gcc option
+*/
+int main(void){
+printf("with\tproper\tgrammer,\tbut\tthe\toutcome\tis\ta\tpiece\tof\tart,");
+return(0);
+}
