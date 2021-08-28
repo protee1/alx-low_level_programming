5cde9fc5eb09b1ad279ca0b219d4b5a6a94c8e41
protee1 <inezaprotogene@gmail.com>
2021-08-28 09:33:35 -0700

    Add 2-alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,17 @@
-/*
- * File: 2-print_alphabet.c
- * Auth: Protogene Mugiraneza
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints the alphabetic
  *
- * Return: Always 0.
+ * Return: Always (Success)
  */
 int main(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
 	putchar('\n');
-
 	return (0);
 }	
