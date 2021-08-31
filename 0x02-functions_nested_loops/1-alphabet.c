65058f994beab59932039e047f03c3d3d00b8fe1
protee1 <inezaprotogene@gmail.com>
2021-08-31 06:28:12 -0700

    Add alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+
+	return(0);
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,7 @@
 #define MAIN_H
 
 int _putchar(char);
+/* 1-alphabet.c */
+void print_alphabet(void);
+
 #endif
