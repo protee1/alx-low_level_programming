9ee85c9e439e81a9713cc57fbf2327d632aedbf3
Protogene Mugiraneza <50316270+protee1@users.noreply.github.com>
2021-08-31 21:16:37 +0200

    Update 0-holberton.c
    Fix betty  coding style issues for 0-holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -15,10 +15,11 @@ int main(void)
 {
         char *c = "Holberton";
 	int i;
-	for(i=0; i<10; i++){
+	
+	for(i=0; i<10; i++)
 	        _putchar(c[i]);
-	}
-	_putchar('\n');
+
+	_putchar ('\n');
 
 	return (0);
 }
