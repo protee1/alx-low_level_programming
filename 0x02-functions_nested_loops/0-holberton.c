2eef3d1ac824c9de592b0186e748baaa2a69d75e
protee1 <inezaprotogene@gmail.com>
2021-08-31 05:54:56 -0700

    Add 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- * main - Print hoberton
- *
- * Return:  Always (0)
- */
-
+*main - prints Holberton
+*Return: 0
+*/
 int main(void)
 {
-         char *c ="Holberton";
-	 int i;
-	 for(i=0; i<10; i++){
-           	 _putchar(c[i]);
-	 }
-	 _putchar('\n');
-	 
-	 return(0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
