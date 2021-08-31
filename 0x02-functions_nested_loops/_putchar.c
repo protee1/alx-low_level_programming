a31d61cdf8ee18624b8cad975fb5397d1b5570a5
protee1 <inezaprotogene@gmail.com>
2021-08-31 08:54:39 -0700

    Add 1-alphabet.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,14 @@
-#include<unistd.h>
+#include <unistd.h>
 #include "main.h"
+
 /**
- * * _putchar -writes the character to stdout
- *  * @c: The character to print
- *   * Return: On success 1.
- *    * On Error, -1 is returned, and errno is set to appropriately.
- *     */
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *       */
 int _putchar(char c)
 {
-           return(write(1, &c, 1));
-}	
+	return (write(1, &c, 1));
+}
