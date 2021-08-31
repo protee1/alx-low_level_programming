00f198ac9d51c052b217f8c3cd2bcc3c0fdc58d3
Protogene Mugiraneza <50316270+protee1@users.noreply.github.com>
2021-08-31 21:22:21 +0200

    Update 0-holberton.c
    Change print from Holberton to _putchar fro 0-holberton question

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,25 +1,21 @@
-/*
- * File: 0-holberton.c
- * Auth: Protogene Mugiraneza
- *
- */
-
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-        char c[10] = "_putchar";
-	int i;
-	
-	for(i=0; i<9; i++)
-	        _putchar(c[i]);
-
-	_putchar ('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
