4f2398307c5329a26578f960b66f538fc76ca3af
protee1 <inezaprotogene@gmail.com>
2021-08-31 05:07:21 -0700

    Add 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Print hoberton
+ *
+ * Return:  Always (0)
+ */
+
+int main(void)
+{
+         char *c ="Holberton";
+	 int i;
+	 for(i=0; i<10; i++){
+           	 _putchar(c[i]);
+	 }
+	 _putchar('\n');
+	 
+	 return(0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+#include "main.h"
+/**
+ * * _putchar -writes the character to stdout
+ *  * @c: The character to print
+ *   * Return: On success 1.
+ *    * On Error, -1 is returned, and errno is set to appropriately.
+ *     */
+int _putchar(char c)
+{
+           return(write(1, &c, 1));
+}	
