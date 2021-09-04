f2f123c0ffceadd33584334876cd0823dea1143e
protee1 <inezaprotogene@gmail.com>
2021-09-04 11:34:31 -0700

    Add 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _upper - checks if input is an uppercase letter in ASCII
- *  @c:integer to check
- *  REturn: 1 if true, 0 if false
+ * _isupper - checks if input is an uppercase letter in ASCII
+ * @c: integer to check
+ * Return: 1 if true, 0 if false
  */
-int _isupper(intc){
-       return (c>=65 && c<=90);
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
 }
