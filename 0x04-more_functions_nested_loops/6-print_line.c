e46b72f7c1e47f7b05688ff71b597f65fd0f6628
protee1 <inezaprotogene@gmail.com>
2021-09-04 13:43:37 -0700

    Add 6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - print line variable length
+ * @n: length of line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	_putchar('\n');
+}
+
