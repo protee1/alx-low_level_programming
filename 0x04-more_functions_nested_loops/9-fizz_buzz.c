cf41e9f4f50920bd36712f8c04ea15856907df00
Protogene Mugiraneza <50316270+protee1@users.noreply.github.com>
2021-09-04 23:44:00 +0200

    Update 9-fizz_buzz.c
    Add 9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,20 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- *  main - prints "Fizz" for numbers divisible by 3,
- *  prints "Buzz" fro numbers divisible by 5,
- *  prints "FizzBu" for numbers divisible by 3 and 5
- *  for numbers 1-100
- *  Return: 0;
+ * main - prints "Fizz" for numbers divisible by 3,
+ * prints "Buzz" for numbers divisible by 5,
+ * prints "FizzBuzz" for numbers divisible by 3 and 5
+ * for numbers 1-100
+ * Return: 0;
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i<= 99; i++)
+	for (i = 1; i <= 99; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuz ");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
@@ -24,3 +24,4 @@ int main(void)
 	}
 	printf("Buzz\n");
 	return (0);
+}
